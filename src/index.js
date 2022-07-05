2095e6577ecb9e49d0db2ceb403558b9ae4e55d4
SAM <samwfelice@gmail.com>
2022-07-05 23:24:14 +0530

    add htmlwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import {v4 as uuidv4} from 'uuid';
 import generateJoke from './modules/generateJoke.js';
+import './styles/main.css'
 
+console.log(uuidv4());
 console.log(generateJoke());
+console.log(1)
